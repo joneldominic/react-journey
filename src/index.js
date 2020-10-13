1efd8f3dd6281812e3424a6073460c765c5b0691
Jonel Dominic Tapang <dneljo@yahoo.com>
2020-10-13 22:35:59 +0800

    Referencing JS Variable

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getMyName() {
+  return 'Jonel';
+}
+
 // Create a react component
 const App = () => {
+  const buttonText = 'Click Me!';
+
   return (
     <div>
       <label className="label" for="name">
         Enter Name:
       </label>
-      <input id="name" type="text"/>
-      <button style={{backgroundColor: 'blue', color: 'red'}}>
-        Submit
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'red' }}>
+        {buttonText + getMyName()}
       </button>
     </div>
   );
