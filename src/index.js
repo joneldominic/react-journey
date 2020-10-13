5a3a94a83880823d711b10e10d6936d67970ebf6
Jonel Dominic Tapang <dneljo@yahoo.com>
2020-10-13 23:08:06 +0800

    Solution Exercise - 1

--- src/index.js
+++ src/index.js
@@ -7,15 +7,24 @@ const App = () => {
   const buttonText = 'Click Me!'; // CAN BE - 123, [1,2,3], ['one', two', three] | CAN'T BE - {}, instead access object property
   const style = { backgroundColor: 'blue', color: 'white' };
 
+  function getTime() {
+    return new Date().toLocaleTimeString();
+  }
+
   return (
     <div>
-      <label className="label" htmlFor="name">
-        Enter Name:
-      </label>
-      <input id="name" type="text" />
-      <button style={style}>
-        {buttonText}
-      </button>
+      <div>
+        <label className="label" htmlFor="name">
+          Enter Name:
+        </label>
+        <input id="name" type="text" />
+        <button style={style}>{buttonText}</button>
+      </div>
+      <hr />
+      <div>
+        <p>Current Time:</p>
+        <h3>{getTime()}</h3>
+      </div>
     </div>
   );
 };
