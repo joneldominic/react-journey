d3c9dc65dd619c41287dcb91b5a3439593a085db
Jonel Dominic Tapang <dneljo@yahoo.com>
2020-10-13 22:44:55 +0800

    Additional - JS variables on JSX

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getMyName() {
-  return 'Jonel';
-}
-
 // Create a react component
 const App = () => {
-  const buttonText = 'Click Me!';
+  const buttonText = 'Click Me!'; // CAN BE - 123, [1,2,3], ['one', two', three] | CAN'T BE - {}, instead access object property
+  const style = { backgroundColor: 'blue', color: 'white' };
 
   return (
     <div>
@@ -16,8 +13,8 @@ const App = () => {
         Enter Name:
       </label>
       <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'red' }}>
-        {buttonText + getMyName()}
+      <button style={style}>
+        {buttonText}
       </button>
     </div>
   );
