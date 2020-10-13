2d1dd87a128127e210e9204c7c6c87a4740daafa
Jonel Dominic Tapang <dneljo@yahoo.com>
2020-10-13 22:28:17 +0800

    JSX basic - style property

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: 'blue', color: 'red'}}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
